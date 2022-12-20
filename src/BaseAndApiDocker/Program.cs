92b713b8b1e2f4034b72762f71524ece824852cf
Vladislav Yevgrafov <vladislav.yevgrafov@ffin.mobi>
2022-12-20 09:18:36 +0600

    rebase

--- src/BaseAndApiDocker/Program.cs
+++ src/BaseAndApiDocker/Program.cs
@@ -1,8 +1,5 @@
 using BaseAndApiDocker.Config;
 using Database;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
-using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,14 +10,14 @@ builder.Services.AddControllers();
 DatabaseConfig.Setup(builder.Services, builder.Configuration);
 ServiceRegistration.Setup(builder.Services, builder.Configuration);
 
+var app = builder.Build();
+
 builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(@"\temp-keys\"))
 .UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration()
 {
     EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
     ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
 });
-
-var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
